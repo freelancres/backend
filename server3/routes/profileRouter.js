0edb8243a108a673677c42ace4e094269e56c00e
Safwan Kher Allah <68548982+safwan-kher@users.noreply.github.com>
2023-06-18 16:57:10 +0200

    Add files via upload

---
+++ server3/routes/profileRouter.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { check } = require('express-validator');
+
+
+const { test,
+    createProfile } = require(
+        "../controllers/profileController"
+    );
+    
+const router = express.Router();
+
+
+/**
+ * @path /api/profiles/create-profile
+ */
+router.post(
+  "/create-profile",
+  [
+    check("user_id", "User ID is required!").notEmpty(),
+    check("bio", "Bio is required!").notEmpty(),
+    check("location", "Location is required!").notEmpty(),
+  ],
+  createProfile
+);
+
+
+
+
+
+
+
+
+
+/**
+ * @path : /api/profiles/test
+ * @public
+ * @description testing the api
+ * 
+ */
+router.get("/test", test);
+
+
+module.exports = router;
